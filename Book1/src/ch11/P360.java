606613bcc5ea37a8653f82bcb15e8d1f22ed3465
ooo3345sjh <ooo3345sjh@gmail.com>
2022-09-13 17:04:00 +0900

    2022/09/13 수업예제 추가

---
+++ Book1/src/ch11/P360.java
@@ -0,0 +1,26 @@
+package ch11;
+
+/*
+ * 날짜 : 2022/09/13
+ * 이름 : 서정현
+ * 내용 : String과 Integer 클래스의 equals()메서드
+ */
+public class P360 {
+
+	public static void main(String[] args) {
+		
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		System.out.println(str1 == str2);
+		System.out.println(str1.equals(str2));
+
+		Integer i1 = new Integer(100);
+		Integer i2 = new Integer(100);
+		
+		System.out.println(i1 == i2);
+		System.out.println(i1.equals(i2));
+		
+	}
+
+}
--- Book1/src/ch11/P361.java
+++ Book1/src/ch11/P361.java
@@ -3,14 +3,14 @@ package ch11;
 /*
  * 날짜 : 2022/09/08
  * 이름 : 서정현
- * 내용 : Object 클래스의 equals() 메서드 사용하기
+ * 내용 : equals() 메서드 재정의하기
  */
-class Student {
+class Student1 {
 	
 	int studentId;
 	String studentName;
 	
-	public Student(int studentId, String studentName) {
+	public Student1(int studentId, String studentName) {
 	 this.studentId = studentId;
 	 this.studentName = studentName;
 	}
@@ -19,15 +19,23 @@ class Student {
 	public String toString() {
 		return studentId + "," + studentName;
 	}
+	
+	@Override
+		public boolean equals(Object obj) {
+			if(!(obj instanceof Student1)) return false;
+			Student1 student = (Student1) obj;
+			return this.studentId == student.studentId;
+	
+		}
 }
 
-public class P358 {
+public class P361 {
 	
 	public static void main(String[] args) {
 		
-		Student studentLee = new Student(100, "이상원");
-		Student studentLee2 = studentLee; // 주소 복사
-		Student studentSang = new Student(100, "이상원");
+		Student1 studentLee = new Student1(100, "이상원");
+		Student1 studentLee2 = studentLee; // 주소 복사
+		Student1 studentSang = new Student1(100, "이상원");
 		
 		if(studentLee == studentLee2) // == 기호로 비교
 			System.out.println("studentLee와 studentLee2의 주소는 같습니다.");
