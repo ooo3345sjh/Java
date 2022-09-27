37ff90185010b52f7f1728b71fba38d3ae31a8c3
ooo3345sjh <ooo3345sjh@gmail.com>
2022-09-27 16:58:03 +0900

    2022/09/27 Java 최종연습문제(2)

--- Book1/src/ch13/P481.java
+++ Book1/src/ch13/P481.java
@@ -6,7 +6,9 @@ import java.util.List;
 /*
  * 날짜 : 2022/09/26
  * 이름 : 서정현
- * 내용 : 스트림 활용하기(1)
+ * 내용 : P481_P482
+ * 	- 스트림 활용하기_1(P481)
+ * 	- 스트림 활용하기_2(P482)
  */
 class TravelCustomer {
 	private String name;
@@ -46,6 +48,26 @@ public class P481 {
 		customerList.add(cutomerLee);
 		customerList.add(cutomerkim);
 		customerList.add(cutomerHong);
+		
+		System.out.println("== 고객 명단 추가된 순서대로 출력 ==");
+		customerList.stream().map(c -> c.getName()).forEach(s -> System.out.println(s));
+		
+		int total = customerList.stream().mapToInt(c -> c.getPrice()).sum();
+		System.out.println("총 여행 비용은 :" + total + "입니다.");
+		System.out.println();
+		
+		System.out.println("== 20세 이상 고객 명단 정렬하여 출력 ==");
+		customerList.stream().filter(c -> c.getAge() >= 20)
+					.map(c -> c.getName()).sorted().forEach(s -> System.out.println(s));
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
